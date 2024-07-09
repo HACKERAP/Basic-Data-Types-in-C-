ad6071255280a39ef438ef18d5736613f2b99407
aman <aman@DESKTOP-L82JUJH>
2024-07-09 21:11:48 +0530

    Add project files.

---
+++ Basic Data Types in C#/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Basic_Data_Types_in_C_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // Integer variable
+            int age = 25;
+            Console.WriteLine("Age: " + age);
+
+
+            // Floating-point variable
+            float height = 5.9f; // Note the 'f' suffix
+            Console.WriteLine("Height: " + height);
+
+            // Double variable
+            double distance = 12345.678;
+            Console.WriteLine("Distance: " + distance);
+
+            // Character variable
+            char grade = 'A';
+            Console.WriteLine("Grade: " + grade);
+
+            // String variable
+            string name = "Alice";
+            Console.WriteLine("Name: " + name);
+
+            // Boolean variable
+            bool isStudent = true;
+            Console.WriteLine("Is Student: " + isStudent);
+
+
+        }
+    }
+}
